e7bef4385e464aa4dde083ae99bd206b9db2890d
bbjarvis <brettbjarvis@gmail.com>
2021-11-07 21:06:06 -0800

    add stat to buttons

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,9 +1,13 @@
 import React from 'react'; //optional
+import { useState } from "react";
 
-function CounterButton(){
+
+function CounterButton(props){
+  const [clickAmount, setClickAmount] = useState(0);
+
   return (
     <button className="CounterButton">
-      You clicked me X amount of times
+      You clicked me {clickAmount} amount of times
     </button>
   );
 }
--- src/components/TextRepeaterButton.js
+++ src/components/TextRepeaterButton.js
@@ -1,9 +1,18 @@
 import React from 'react'; //optional
+import { useState } from "react";
 
-function TextRepeaterButton(){
+
+function TextRepeaterButton(props){
+  const [repetitions, setRepetitions] = useState(1);
+
+  const textArray = [];
+  for (let i = 0; i < repetitions; i++) {
+    textArray.push(<span key={i}>I like this text</span>);
+  }
+  // ...
   return (
     <button className="TextRepeaterButton">
-      <span>I like this text</span>
+      {textArray}
     </button>
   );
 }
