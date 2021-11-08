86e2486d783018166175fbfcdf129c70f2522ffe
bbjarvis <brettbjarvis@gmail.com>
2021-11-07 21:29:10 -0800

    add state functionality to buttons

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -3,9 +3,16 @@ import { useState } from "react";
 
 function AngryButton(props){
   const [anger, setAnger] = useState(0);
+  const hangleClick = () => {
+    if (anger < 1) {
+      setAnger(anger + 0.1);
+    } else {
+      setAnger(0)
+    }
+  }
 
   return (
-    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} className="AngryButton">
+    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} onClick={hangleClick} className="AngryButton">
     {anger < 1 && <span>Don't click me too much! </span>}
     {anger > 1 && <span>Rawr!</span>}
     </button>
